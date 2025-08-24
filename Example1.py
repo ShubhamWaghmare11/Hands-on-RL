1d66c3eacc63871c21bd251195cab45fbd47b28d
maskgamer <gamermask64@gmail.com>
2025-08-24 18:59:22 +0530

    .

--- Example1.py
+++ Example1.py
@@ -1,14 +1,13 @@
 import gymnasium as gym
-
+import time
 
 #Creating the environment
 env = gym.make("LunarLander-v3", render_mode="human")
 
 env.reset()
 
-for step in range(10):
+for step in range(100):
     env.render()
     env.step(env.action_space.sample())
-
 
 env.close()
